2e660b6cd12c4abea4ffac2cd337319eca0c3c99
codygman <cody@codygman.dev>
2021-03-05 15:45:04 -0600

    add back debugging

--- src/Data/Pool/Acquire.hs
+++ src/Data/Pool/Acquire.hs
@@ -4,10 +4,15 @@ module Data.Pool.Acquire (poolToAcquire) where
 
 import Data.Pool (Pool, destroyResource, putResource, takeResource)
 import Data.Acquire (Acquire, ReleaseType(..), mkAcquireType)
+import Debug.Trace
+
+
+debug :: c -> String -> c
+debug = flip trace
 
 -- | Convert a 'Pool' into an 'Acquire'.
 poolToAcquire :: Pool a -> Acquire a
-poolToAcquire pool = fst <$> mkAcquireType getResource freeResource
+poolToAcquire pool = fst <$> mkAcquireType getResource (freeResource `debug` "what is the pool state")
   where
     getResource = takeResource pool
     freeResource (resource, localPool) = \case
