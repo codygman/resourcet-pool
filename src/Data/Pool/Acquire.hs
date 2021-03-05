22a751081c13d816b0424174899d913eca424fbb
codygman <cody@codygman.dev>
2021-03-05 15:43:10 -0600

    Revert "try debugging pool state"
    This reverts commit f1d0f732ee4019efebc2770dddfb1631eeb73a70.

--- src/Data/Pool/Acquire.hs
+++ src/Data/Pool/Acquire.hs
@@ -4,14 +4,10 @@ module Data.Pool.Acquire (poolToAcquire) where
 
 import Data.Pool (Pool, destroyResource, putResource, takeResource)
 import Data.Acquire (Acquire, ReleaseType(..), mkAcquireType)
-import Debug.Trace
-
-
-debug = flip trace
 
 -- | Convert a 'Pool' into an 'Acquire'.
 poolToAcquire :: Pool a -> Acquire a
-poolToAcquire pool = fst <$> mkAcquireType (takeResource pool) (freeResource `debug` "what is the pool state")
+poolToAcquire pool = fst <$> mkAcquireType getResource freeResource
   where
     getResource = takeResource pool
     freeResource (resource, localPool) = \case
